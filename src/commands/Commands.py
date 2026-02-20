762a264544a222464414b81e35ea6b8cc5c6c147
MeIsGaming <info@meisgaming.net>
2026-02-20 01:05:10 +0100

    Refactor command registry to improve metadata structure and enhance command loading

--- src/commands/Commands.py
+++ src/commands/Commands.py
@@ -1,78 +1,85 @@
 """Command registry, lookup, and autocomplete helpers."""
 
+from importlib import import_module
 from functools import lru_cache
 
 from Config import getCommandName
 
-from .AddToQueue import AddToQueue
-from .Artist import Artist
-from .CurrentTrackInfo import CurrentTrackInfo
-from .DecreaseVolume import DecreaseVolume
-from .EditConfig import EditConfig
-from .FastForward import FastForward
-from .IncreaseVolume import IncreaseVolume
-from .Login import Login
-from .Logout import Logout
-from .Next import Next
-from .Pause import Pause
-from .Play import Play
-from .Previous import Previous
-from .ReloadConfig import ReloadConfig
-from .Repeat import Repeat
-from .Resume import Resume
-from .Rewind import Rewind
-from .Seek import Seek
-from .SetVolume import SetVolume
-from .Shuffle import Shuffle
-from .Song import Song
-
 
 def _build_command_specs():
-    """Return command registry: (config key, command class, short description)."""
+    """Return command registry metadata."""
     return [
-        ("NEXT_COMMAND", Next, "Skip to next track"),
-        ("PREVIOUS_COMMAND", Previous, "Go to previous track"),
-        ("PAUSE_COMMAND", Pause, "Pause current playback"),
-        ("RESUME_COMMAND", Resume, "Resume playback"),
-        ("DECREASE_VOLUME_COMMAND", DecreaseVolume, "Decrease volume"),
-        ("INCREASE_VOLUME_COMMAND", IncreaseVolume, "Increase volume"),
-        ("PLAY_COMMAND", Play, "Play song, artist or playlist"),
-        ("ADD_TO_QUEUE_COMMAND", AddToQueue, "Add song to queue"),
-        ("LOGOUT_COMMAND", Logout, "Logout from Spotify"),
-        ("LOGIN_COMMAND", Login, "Login to Spotify"),
-        ("CURRENT_TRACK_INFO_COMMAND", CurrentTrackInfo, "Show current track info"),
-        ("RELOAD_CONFIG_COMMAND", ReloadConfig, "Reload configuration"),
-        ("EDIT_CONFIG_COMMAND", EditConfig, "Open configuration file"),
-        ("SHUFFLE_COMMAND", Shuffle, "Toggle shuffle"),
-        ("REPEAT_COMMAND", Repeat, "Set repeat mode"),
-        ("FAST_FORWARD_COMMAND", FastForward, "Seek forward"),
-        ("REWIND_COMMAND", Rewind, "Seek backward"),
-        ("SEEK_COMMAND", Seek, "Seek to a position"),
-        ("SET_VOLUME_COMMAND", SetVolume, "Set absolute volume"),
-        ("PLAY_ARTIST_COMMAND", Artist, "Play top tracks by artist"),
-        ("PLAY_SONG_COMMAND", Song, "Play a song"),
+        ("NEXT_COMMAND", "Next", "Next", "Skip to next track"),
+        ("PREVIOUS_COMMAND", "Previous", "Previous", "Go to previous track"),
+        ("PAUSE_COMMAND", "Pause", "Pause", "Pause current playback"),
+        ("RESUME_COMMAND", "Resume", "Resume", "Resume playback"),
+        (
+            "DECREASE_VOLUME_COMMAND",
+            "DecreaseVolume",
+            "DecreaseVolume",
+            "Decrease volume",
+        ),
+        (
+            "INCREASE_VOLUME_COMMAND",
+            "IncreaseVolume",
+            "IncreaseVolume",
+            "Increase volume",
+        ),
+        ("PLAY_COMMAND", "Play", "Play", "Play song, artist or playlist"),
+        ("ADD_TO_QUEUE_COMMAND", "AddToQueue", "AddToQueue", "Add song to queue"),
+        ("LOGOUT_COMMAND", "Logout", "Logout", "Logout from Spotify"),
+        ("LOGIN_COMMAND", "Login", "Login", "Login to Spotify"),
+        (
+            "CURRENT_TRACK_INFO_COMMAND",
+            "CurrentTrackInfo",
+            "CurrentTrackInfo",
+            "Show current track info",
+        ),
+        ("RELOAD_CONFIG_COMMAND", "ReloadConfig", "ReloadConfig", "Reload configuration"),
+        ("EDIT_CONFIG_COMMAND", "EditConfig", "EditConfig", "Open configuration file"),
+        ("SHUFFLE_COMMAND", "Shuffle", "Shuffle", "Toggle shuffle"),
+        ("REPEAT_COMMAND", "Repeat", "Repeat", "Set repeat mode"),
+        ("FAST_FORWARD_COMMAND", "FastForward", "FastForward", "Seek forward"),
+        ("REWIND_COMMAND", "Rewind", "Rewind", "Seek backward"),
+        ("SEEK_COMMAND", "Seek", "Seek", "Seek to a position"),
+        ("SET_VOLUME_COMMAND", "SetVolume", "SetVolume", "Set absolute volume"),
+        ("PLAY_ARTIST_COMMAND", "Artist", "Artist", "Play top tracks by artist"),
+        ("PLAY_SONG_COMMAND", "Song", "Song", "Play a song"),
     ]
 
 
 @lru_cache(maxsize=1)
-def _build_command_map():
+def _build_command_module_map():
     command_map = {}
-    for setting_name, command_class, _ in _build_command_specs():
-        command_map[getCommandName(setting_name)] = command_class
+    for setting_name, module_name, class_name, _ in _build_command_specs():
+        command_map[getCommandName(setting_name)] = (module_name, class_name)
+    return command_map
+
+
+@lru_cache(maxsize=1)
+def _build_command_name_map():
+    command_map = {}
+    for setting_name, _, _, _ in _build_command_specs():
+        command_map[getCommandName(setting_name)] = setting_name
     return command_map
 
 
 @lru_cache(maxsize=1)
 def _build_command_descriptions():
     descriptions = {}
-    for setting_name, _, description in _build_command_specs():
+    for setting_name, _, _, description in _build_command_specs():
         descriptions[getCommandName(setting_name)] = description
     return descriptions
 
 
+def _load_command_class(module_name: str, class_name: str):
+    module = import_module(f".{module_name}", package=__package__)
+    return getattr(module, class_name)
+
+
 def getCommandNames():
     """Return all command names sorted for autocomplete and lookup."""
-    return sorted(_build_command_map())
+    return sorted(_build_command_name_map())
 
 
 def autocompleteMatches(command_prefix: str):
@@ -98,7 +105,9 @@ def autocompleteMatches(command_prefix: str):
 
 def executeCommand(command, spotify):
     """Instantiate command handler for a normalized command token."""
-    command_map = _build_command_map()
-    if command in command_map:
-        return command_map[command](spotify)
+    command_name_map = _build_command_name_map()
+    if command in command_name_map:
+        module_name, class_name = _build_command_module_map()[command]
+        command_class = _load_command_class(module_name, class_name)
+        return command_class(spotify)
     raise RuntimeError(f"Unknown command: {command}")
